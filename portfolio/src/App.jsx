fdffabeb6ecbe0e69258a7f9a29fed577c806e63
Siddharth Sharma <siddharth28setu@gmail.com>
2024-07-28 20:28:17 +0530

    bg change

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -11,8 +11,19 @@ function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed top-0 z-[-110] h-full w-full">
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      </div>
+      
+      
+      
+      <div class="relative size-full bg-slate-950">
+  <div
+    class="absolute bottom-0 left-[-20%] right-0 top-[-10%] size-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"
+  ></div>
+  <div
+    class="absolute bottom-0 right-[-20%] top-[-10%] size-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"
+  ></div>
+</div>
+      
+       </div>
       
       
       <div className="container mx-auto px-8">
