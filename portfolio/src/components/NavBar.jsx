4a051ba033b8af9b5f3627ecd040dbcfe116e6fc
Siddharth Sharma <siddharth28setu@gmail.com>
2024-07-28 16:01:16 +0530

    insta link updated

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const navbar = () => {
         </div>
        <div className="m-8 flex item-center justify-center gap-4 text-2xl" >
         
-      <a href="https://www.instagram.com/sid_since2003//">
+      <a href="https://www.instagram.com/sid_since2003/">
       <FaInstagram />
       </a>
 
